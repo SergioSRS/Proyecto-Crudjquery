8f9780df33f931d3d0e327092cceb0259e93f3d2
SergioSRS <sergioriverasalgado15@gmail.com>
2023-01-27 08:06:37 +0100

    informe de usabilidad y aria casi completo

--- src/www/js/vistas/vistainicio.js
+++ src/www/js/vistas/vistainicio.js
@@ -8,7 +8,7 @@ export class VistaInicio extends Vista{
 	/**
 		Constructor de la clase
 	**/
-	constructor(div,controlador){
+	constructor(div,controlador,i){
 		super(div)
 		//Hacemos que la VistaCRUD "observe" al Modelo
 		this.controlador = controlador
@@ -29,6 +29,12 @@ export class VistaInicio extends Vista{
 	
 		//Tabla
 		this.tabla = this.div.find('tbody').eq(0)
+
+		//Aemet
+		this.divDatosAemet = $("<div></div>")
+
+		//Para generar tabindex
+		this.i=0;
 		
 	}
 	pulsarAnadir(){
@@ -49,7 +55,7 @@ export class VistaInicio extends Vista{
 		if(datos != null)
 		{
 			for (let dato of datos){
-
+				console.log(this.i)
 				let tr = $("<tr></tr>")
 				
 				this.tabla.append(tr)
@@ -78,22 +84,35 @@ export class VistaInicio extends Vista{
 				let spanEliminar = $("<span></span>")
 				td3.append(spanEliminar)
 				spanEliminar.addClass('icono')
+				spanEliminar.attr('role','button')
+				spanEliminar.attr('tabindex', this.i)
+				spanEliminar.attr('aria-pressed', 'false')
 				spanEliminar.text('🗑') 
+				spanEliminar.css('cursor','pointer')
 				spanEliminar.on('click', this.eliminar.bind(this, dato.id)) 
-				
+				this.i++;
 			
 				let spanConsultar = $("<span></span>")
 				td3.append(spanConsultar)
 				spanConsultar.addClass('icono')
+				spanConsultar.attr('role','button')
+				spanConsultar.attr('tabindex', this.i)
+				spanConsultar.attr('aria-pressed', 'false')
 				spanConsultar.text('🔎')
+				spanConsultar.css('cursor','pointer')
 				spanConsultar.on('click',this.consultar.bind(this, dato))
 			
-			
+				this.i++;
 				let spanEditar = $("<span></span>")
 				td3.append(spanEditar)
 				spanEditar.addClass('icono')
+				spanEditar.attr('role','button')
+				spanEditar.attr('tabindex', this.i)
+				spanEditar.attr('aria-pressed', 'false')
 				spanEditar.text('✏')
+				spanEditar.css('cursor','pointer')
 				spanEditar.on('click',this.editar.bind(this, dato))
+				this.i++;
 				
 		}
 		if(datos.length==0)
@@ -109,15 +128,17 @@ export class VistaInicio extends Vista{
 	}
 	}
 	footerAemet(){
-		let datos = this.modelo.getDatosTiempo()
-		let divDatosAemet = $("<div></div>")
-
-		this.div.append(divDatosAemet)
-		divDatosAemet.css('white-space', 'pre-line')
-		divDatosAemet.css('text-align','center')
 		
-		divDatosAemet.text(datos)
-
+		this.borrarAemet()
+		let datos = this.modelo.getDatosTiempo()
+		
+		this.div.append(this.divDatosAemet)
+		this.divDatosAemet.css('white-space', 'pre-line')
+		this.divDatosAemet.css('text-align','center')
+		this.divDatosAemet.css('margin-top','200px')
+	
+		
+		this.divDatosAemet.text(datos)
 
 	}
 	/**
@@ -127,17 +148,23 @@ export class VistaInicio extends Vista{
 		while (this.tabla.children().length>0)
 		this.tabla.children().first().remove()
 	}
+	borrarAemet(){
+		this.divDatosAemet.remove()
+	}
 	/** 
 	 * Metodo para consultar un registro 
 	*/
-	consultar(dato){	
+	consultar(dato){
+		this.i=0;	
 		this.controlador.pulsarConsulta(dato);
 	}
 	eliminar(id){	
+		this.i=0;
 		this.controlador.eliminarVideojuego(id)
 		this.actualizar();
 	}
 	editar(dato){
+		this.i=0;
 		this.controlador.pulsarModificar(dato);
 		this.actualizar();
 	}
